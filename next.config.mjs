f2cc5aee6467846b6c3549b6ad73d84815bf472b
Gustavo Nobre <gustavognobre@gmail.com>
2024-09-29 21:56:09 -0300

    server image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Adiciona a configuração de exportação estática
-  };
-  
-  export default nextConfig;
-  
+  output: 'export', // Configuração para exportação estática
+  images: {
+    domains: ['viacaoavanti.com.br'], // Domínio permitido para imagens
+    unoptimized: true, // Desabilita a otimização de imagens para exportação estática
+  },
+};
+
+export default nextConfig;
